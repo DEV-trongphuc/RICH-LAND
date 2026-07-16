9b96a16e8e0b2b20a66869e2cbe850d439572bbd
DEV-trongphuc <DEV-trongphuc@users.noreply.github.com>
2026-07-16 21:18:56 +0700

    deploy: automated deployment and push

--- backend/show_columns.php
+++
@@ -1,7 +0,0 @@
-<?php
-include __DIR__ . '/db_connect.php';
-
-$res = $conn->query("SHOW COLUMNS FROM contacts");
-while ($row = $res->fetch_assoc()) {
-    echo $row['Field'] . " - " . $row['Type'] . "\n";
-}
