3a50588fa489eec8e1bc0753e6d6deaae00b701b
DEV-trongphuc <DEV-trongphuc@users.noreply.github.com>
2026-07-19 23:12:54 +0700

    deploy: automated deployment and push

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -232,7 +232,7 @@ export const Sidebar = ({ isCollapsed, onToggleCollapse, isMobileOpen, onMobileC
         onMouseLeave={() => setIsHovered(false)}
         style={{
           width: isCollapsed ? 60 : 220,
-          background: 'linear-gradient(135deg, #181515 0%, #381f21 50%, #121010 100%)',
+          background: 'linear-gradient(135deg, #110d0e 0%, #221213 50%, #0d0b0b 100%)',
           color: '#dadada',
           display: 'flex',
           flexDirection: 'column',
