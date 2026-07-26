b7d9871086e3656fd6b03ca4b5fccc840184a5b8
DEV-trongphuc <DEV-trongphuc@users.noreply.github.com>
2026-07-26 14:40:48 +0700

    Add Powered by Turniodev console log

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log('%cPower by Turniodev (https://fb.com/turni0)', 'color: red; font-weight: bold; font-size: 14px;');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
